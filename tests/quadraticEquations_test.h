a556f6e0e003c0ca8114d640c6d0ea746a10badc
x2late2take <cookeechie@yandex.ru>
2024-05-31 12:23:10 +0300

    123

--- tests/quadraticEquations_test.h
+++ tests/quadraticEquations_test.h
@@ -29,3 +29,21 @@ TEST(quadraticEquations_test, negativeDiscriminant) {
     ASSERT_EQ(roots.numRoots, 0);
 }
 
+TEST(quadraticEquations_test, positiveDiscriminantLarge) {
+    Roots roots = quadraticEquations(2, -11, 5);
+    ASSERT_EQ(roots.numRoots, 2);
+    ASSERT_NEAR(roots.root1, 5.0, 0.001);
+    ASSERT_NEAR(roots.root2, 0.5, 0.001);
+}
+
+TEST(quadraticEquations_test, zeroDiscriminantLarge) {
+    Roots roots = quadraticEquations(3, -18, 27);
+    ASSERT_EQ(roots.numRoots, 1);
+    ASSERT_DOUBLE_EQ(roots.root1, 3);
+    ASSERT_DOUBLE_EQ(roots.root2, 3);
+}
+
+TEST(quadraticEquations_test, negativeDiscriminantLarge) {
+    Roots roots = quadraticEquations(1, -3, 10);
+    ASSERT_EQ(roots.numRoots, 0);
+}
